6de0145ef7f800131cfc163231331394250eaa1c
David Garcia Jeronimo <davidgarcia2809@gmail.com>
2025-08-02 17:34:43 -0600

    Agregue compatibilidad con JWT y relaciones al modelo de usuario
    Se implementó la interfaz JWTSubject para la autenticación JWT, se añadieron relaciones con los modelos Mechanicals y type_user, se establecieron las marcas de tiempo como falsas y se actualizaron los atributos rellenables. Estos cambios mejoran la autenticación y las asociaciones de modelos.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,15 +7,21 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Laravel\Cashier\Billable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable, Billable;
+
     protected $primaryKey = 'users_id';
+    public $timestamps = false;
     protected $fillable = [
         'name',
         'email',
         'password',
+        'last_name',
+        'type_users_id',
+
     ];
 
     protected $hidden = [
@@ -28,6 +34,35 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
+    public function mechanicalWorkshop()
+    {
+        return $this->hasOne(Mechanicals::class, 'users_id', 'users_id');
+    }
+
+    public function typeUser()
+    {
+        return $this->belongsTo(type_user::class, 'type_users_id', 'type_users_id');
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     // Verificar si el usuario tiene una suscripción activa
     public function hasActiveSubscription(): bool
     {
